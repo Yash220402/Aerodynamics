23ca03e2b0f04e79c388c345f81183b908cd2589
Yash220402 <yash220402@gmail.com>
2023-06-02 00:18:42 +0530

    create meshgrid

--- source_sink.py
+++ source_sink.py
@@ -1,1 +1,12 @@
+import math 
+import numpy
+import matplotlib as plt
 
+N = 50
+x_start, x_end = -2.0, 2.0
+y_start, y_end = -1.0, 1.0
+
+x = numpy.linspace(x_start, x_end, N)
+y = numpy.linspace(y_start, y_end, N)
+
+X, Y = numpy.meshgrid(x, y)
