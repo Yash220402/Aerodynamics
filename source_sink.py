1836c10e14f4ec22dd5c21b3fe7af3687a9f7af3
Yash220402 <yash220402@gmail.com>
2023-06-02 00:29:32 +0530

    visualise valocity components

--- source_sink.py
+++ source_sink.py
@@ -1,6 +1,7 @@
 import math 
 import numpy
 import matplotlib as plt
+import matplotlib.pyplot as plt
 
 N = 50
 x_start, x_end = -2.0, 2.0
@@ -9,4 +10,13 @@ y_start, y_end = -1.0, 1.0
 x = numpy.linspace(x_start, x_end, N)
 y = numpy.linspace(y_start, y_end, N)
 
-X, Y = numpy.meshgrid(x, y)
+X, Y = numpy.meshgrid(x, y)  # create meshgrid
+
+width = 10
+height = (y_end - y_start) / (x_end - x_start) * width
+plt.xlabel('x', fontsize=16)
+plt.ylabel('y', fontsize=16)
+plt.xlim(x_start, x_end)
+plt.ylim(y_start, y_end)
+plt.scatter(X, Y, s=5, color="red", marker='o')
+plt.show()
